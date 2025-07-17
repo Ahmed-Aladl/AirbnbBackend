2b83eb0063564c2358566135a2828b03c6403e79
mahmoud140106 <amer140106@gmail.com>
2025-07-17 23:20:53 +0300

    ApiResponse

---
+++ Application/Responses/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Responses
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+
+        public static ApiResponse<T> Success(T data, string message = "")
+            => new() { IsSuccess = true, Message = message, Data = data };
+
+        public static ApiResponse<T> Fail(string message)
+            => new() { IsSuccess = false, Message = message, Data = default };
+    }
+}
