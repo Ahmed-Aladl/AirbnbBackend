366b38686e800185eb8af05f89346133a4697ee9
Ahmed Essam <ahmed.essam.aladl@gmail.com>
2025-07-17 23:54:27 +0300

    UnitOfWork

---
+++ Infrastructure/Common/Repositories/Repository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces.IRepositories;
+using Infrastructure.Contexts;
+
+namespace Infrastructure.Common.Repositories
+{
+    public class Repository<TEntity,TKey> : IRepository<TEntity,TKey> where TEntity : class
+    {
+        public AirbnbContext Db { get; set; }
+        public Repository(AirbnbContext _db)
+        {
+            Db = _db;
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return Db.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(TKey id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public void Add(TEntity entity)
+        {
+            Db.Set<TEntity>().Add(entity);
+        }
+        public void Update(TEntity entity)
+        {
+            Db.Entry<TEntity>(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        }
+        public void Delete(TEntity entity)
+        {
+            Db.Set<TEntity>().Remove(entity);
+        }
+
+
+        //public List<TEntity> GetPage<TOrder>(Expression<Func<TEntity, bool>> filterExp, Expression<Func<TEntity, TOrder>> orderExp, string search = "", int page = 1, int pageSize = 10) where TOrder : IComparable<TOrder>
+        //{
+        //    Console.WriteLine("********************************************************************\nBegin");
+        //    IQueryable<TEntity> result = Db.Set<TEntity>().AsNoTracking();
+
+        //    if (!string.IsNullOrEmpty(search))
+        //        result = result.Where(filterExp);
+        //    int courseCount = result.Count();
+
+        //    result = result.OrderBy(orderExp)
+        //                    .Skip((page - 1) * pageSize)
+        //                    .Take(pageSize);
+
+
+
+        //    return result.ToList();
+        //}
+
+
+    }
+}
